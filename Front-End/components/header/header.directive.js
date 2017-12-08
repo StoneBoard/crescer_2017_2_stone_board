dc2c0e1d0f97b0d3ab1e57e932e9d1064b535f50
Willian <willian.pazinatto@cwi.com.br>
2017-12-08 13:25:47 -0200

    Refactory(TCC-StoneBoard): corrigindo erro no index.html

---
+++ Front-End/components/header/header.directive.js
@@ -0,0 +1,37 @@
+(function(){
+  'use strict';
+
+  angular.module('stoneBoard')
+	.directive('navBarHeader', function ($httpProvider,authService, $rootScope) {
+
+    return {
+
+      restrict: 'E',
+
+      scope: {},
+
+      templateUrl: 'components/header/headerSuperior.html',
+
+      controller: function ($scope) {
+
+        atualizarUsuario();
+
+        $scope.logout = authService.logout;
+        $scope.usuario = authService.getUsuario();
+        $rootScope.$on('authLoginSuccess', function () {
+          atualizarUsuario();
+        });
+
+        $rootScope.$on('authLogoutSuccess', function () {
+          atualizarUsuario();
+        });
+
+        function atualizarUsuario() {
+          $scope.usuario = authService.getUsuario();
+        }
+      }
+    }
+
+  });
+  
+});()
--- Front-End/module.js
+++ Front-End/module.js
@@ -2,6 +2,27 @@
   'use strict';
 
   angular
-    .module('stoneBoard', ['ngStorage']);
+    .module('stoneBoard', ['ngStorage','ngRoute']);
+
+
+
+  angular
+    .module('stoneBoard').constant('authConfig', {
+
+      // Obrigatória - URL da API que retorna o usuário
+
+      //urlUsuario: 'http://10.99.0.12:3296/api/acessos/usuarioLogado',
+      //urlUsuario: 'http://10.99.0.24/AutDemo.WebApi/api/acessos/usuariologado',
+
+      urlUsuario: 'http://localhost:9090/api/acessos/usuario',
+
+      urlLogin: '/login',
+
+      urlPrivado: '/privado',
+
+      urlLogout: '/home'
+  });
+
+
 
 })();
