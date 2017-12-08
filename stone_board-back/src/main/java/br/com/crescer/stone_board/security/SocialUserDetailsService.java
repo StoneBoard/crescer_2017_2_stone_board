b2a0292141efcc3f5a5634b0f20fd68676bbeb4a
Júlia Vila <juliafvila@gmail.com>
2017-12-08 15:30:22 -0200

    feat(stoneboard): implementando login na api
    por ora, senha padrão "1234"

--- stone_board-back/src/main/java/br/com/crescer/stone_board/security/SocialUserDetailsService.java
+++ stone_board-back/src/main/java/br/com/crescer/stone_board/security/SocialUserDetailsService.java
@@ -1,13 +1,17 @@
 package br.com.crescer.stone_board.security;
 
+import br.com.crescer.stone_board.entity.Person;
+import br.com.crescer.stone_board.service.PersonService;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,13 +20,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class SocialUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private PersonService personService;
+    
+    PasswordEncoder encoder = new BCryptPasswordEncoder(); 
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        final List<GrantedAuthority> grants = new ArrayList<>();
-        if ("admin".equals(username)) {
-            grants.add(() -> "ROLE_ADMIN");
+        
+        final Person person = personService.findByEmail(username);
+        if (person == null) {
+                return null;
         }
-        return new User(username, new BCryptPasswordEncoder().encode("password"), grants);
-    }
+        
+        final List<GrantedAuthority> grants = new ArrayList<>();
 
+        //return new User(person.getEmail(), person.getPass(), grants);
+        return new User(person.getEmail(), encoder.encode("1234"), grants);
+    }
 }
