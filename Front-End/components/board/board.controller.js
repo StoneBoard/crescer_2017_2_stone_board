46e497a9794b73dc5df4feca057e16c58d89b81a
Júlia Vila <juliafvila@gmail.com>
2017-12-09 17:12:04 -0200

    feat(stoneboard): implementando board e post its
    front

--- Front-End/components/board/board.controller.js
+++ Front-End/components/board/board.controller.js
@@ -3,8 +3,37 @@
 
   angular
   .module('stoneBoard')
-  .controller('controllerBoard', function ($window) {
+  .controller('controllerBoard', function ($scope, $window) {
 
+  	//console.log($window);
+  	$scope.rowStyle = {};
+
+  	let idealWidth = 550; //px
+  	let numSession = 4;
+  	let newWidth;
+
+  	resizeRow();
+
+  	// verifica se houve alguma mudança na largura da tela e recalcula largura do board
+		angular.element($window).bind('resize', resizeRow);
+  	
+  	function resizeRow() {
+
+  		let rowSession = angular.element(document.getElementById('row-sessions'));
+  		rowSession.css('width', '100%');
+	  	
+	  	if (idealWidth > rowSession[0].clientWidth) {
+	  		newWidth = $window.innerWidth * numSession + 'px';
+	  	}
+	  	else if ( (idealWidth * numSession) >= rowSession[0].clientWidth ) {
+	  		newWidth =	(idealWidth * numSession) + 'px';
+	  	}
+	  	else {
+	  		newWidth = '100%';
+	  	}
+
+	  	rowSession.css('width', newWidth);	  		
+  	}
 
   });
 
---
+++ Front-End/components/postit/postit.directive.js
@@ -0,0 +1,23 @@
+(function(){
+  'use strict';
+
+  angular.module('stoneBoard')
+		.directive('postIt', function () {
+
+			return {
+
+      restrict: 'E',
+
+      scope: {},
+
+      templateUrl: 'components/postit/postit.html',
+
+      controller: function ($scope) {
+
+      }
+
+    }
+
+	});
+
+})();
