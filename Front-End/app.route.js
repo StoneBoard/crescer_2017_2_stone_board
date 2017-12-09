73af2b26713a0ea7142dc99f9df80c2a4d3c953f
Willian Pazinatto <willianpazinatto@hotmail.com>
2017-12-09 13:21:58 -0200

    Fix(TCC-StoneBoard):Corrigindo Merge

--- Front-End/app.route.js
+++ Front-End/app.route.js
@@ -21,17 +21,12 @@
           }
         }*/
       })
-<<<<<<< HEAD
-      .otherwise('/login');
-=======
 
       .when('/board', {
         controller: 'controllerBoard',
         templateUrl: 'components/board/board.html'
-      })
+      }).otherwise('/login');
 
-      .otherwise('/');
->>>>>>> 65ee3a26827c8c5678ccc4fe608e19fbf30c1057
   });
 
 })();
