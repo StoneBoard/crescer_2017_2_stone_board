0402f862a9f1b6b62f77742809383794423d53bc
Júlia Vila <juliafvila@gmail.com>
2017-12-08 22:28:48 -0200

    feat(stoneboard): implementando card do dashboard

---
+++ Front-End/components/card/card.directive.js
@@ -0,0 +1,26 @@
+(function(){
+  'use strict';
+
+  angular.module('stoneBoard')
+		.directive('cardDashBoard', function () {
+
+			return {
+
+      restrict: 'E',
+
+      scope: {},
+
+      templateUrl: 'components/card/card.html',
+
+      controller: function ($scope) {
+
+
+
+
+      }
+
+    }
+
+	});
+
+})();
