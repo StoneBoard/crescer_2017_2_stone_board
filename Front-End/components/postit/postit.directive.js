127a9229473082a81d61865c60a878b6fae6bce5
Júlia Vila <juliafvila@gmail.com>
2017-12-11 16:26:24 -0200

    feat(stoneboard): implementando interface de edição de card

--- Front-End/components/postit/postit.directive.js
+++ Front-End/components/postit/postit.directive.js
@@ -15,7 +15,34 @@
 
       templateUrl: 'components/postit/postit.html',
 
-      controller: function ($scope) {
+      controller: function ($scope, authService) {
+
+        let usuario = authService.getUsuario();
+        $scope.isWriter = usuario.id === $scope.p.id_writer;
+        $scope.paddingCard = $scope.isWriter ? {} : {'padding-top': '20px'};
+        
+        $scope.editMode = false;
+        let oldText = "";
+
+        $scope.changeMode = function(editing) {
+          if (editing) oldText = JSON.parse(JSON.stringify($scope.p.text));
+          $scope.editMode = editing;
+        }
+
+        $scope.delete = function(){
+          console.log('delete');
+        }
+
+        $scope.update = function() {
+          console.log('update');
+          $scope.changeMode(false);
+        }
+
+        $scope.undo = function() {
+          console.log('undo');
+          $scope.p.text = JSON.parse(JSON.stringify(oldText));
+          $scope.changeMode(false);
+        }
 
       }
 
