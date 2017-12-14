d1cbd71bd90901244ab5a1fdae7ec3740fca9dc5
Júlia Vila <juliafvila@gmail.com>
2017-12-14 15:00:49 -0200

    feat(stoneboard): implementando interface de Grupo de Resultado.

--- Front-End/components/postit/postit.directive.js
+++ Front-End/components/postit/postit.directive.js
@@ -10,8 +10,7 @@
 
       scope: {
         sendMessage : '=send',
-        p : '=info',
-        color : '=color',
+        postIt : '=info',
         boardStatus : '=boardStatus',
         cardMessages : '=cardMessages'
       },
@@ -19,43 +18,48 @@
       templateUrl: 'components/postit/postit.html',
 
       controller: function ($scope, authService,postitService,toastr, voteService) {
+
+        console.log($scope.postIt)
+        let colorPallet = ['orange', 'blue', 'pink', 'green'];
+        $scope.color = colorPallet[$scope.postIt.color];
+
         let usuario = authService.getUsuario();
-        console.log($scope.boardStatus);
-        $scope.vPositivo = $scope.p.votes.filter(v => v.positive);
-        $scope.vNegativo = $scope.p.votes.filter(v => !v.positive);
+
+        $scope.vPositivo = $scope.postIt.votes.filter(v => v.positive);
+        $scope.vNegativo = $scope.postIt.votes.filter(v => !v.positive);
 
         $scope.votoPositivo = $scope.vPositivo.filter(v => v.id_person == usuario.id).length > 0 ? 'icon-selected' : '';
         $scope.votoNegativo = $scope.vNegativo.filter(v => v.id_person == usuario.id).length > 0 ? 'icon-selected' : '';
 
-        $scope.isWriter = usuario.id === $scope.p.id_writer;
-        $scope.paddingCard = $scope.isWriter ? {} : {'padding-top': '20px'};
+        $scope.isWriter = usuario.id === $scope.postIt.id_writer;
+        $scope.postItaddingCard = $scope.isWriter ? {} : {'padding-top': '20px'};
 
         $scope.editMode = false;
         let oldText = "";
 
         $scope.changeMode = function(editing) {
-          if (editing) oldText = JSON.parse(JSON.stringify($scope.p.text));
+          if (editing) oldText = JSON.parse(JSON.stringify($scope.postIt.text));
           $scope.editMode = editing;
         }
 
         $scope.deleteCard = function(){
-          $scope.cardMessages.deleteCard($scope.p);
+          $scope.cardMessages.deleteCard($scope.postIt);
         }
 
         $scope.update = function() {
           console.log('update')
-          console.log($scope.p)
-          postitService.editCard($scope.p).then();
+          console.log($scope.postIt)
+          postitService.editCard($scope.postIt).then();
           $scope.editMode = false;
         }
         $scope.vote = function(_positive) {
-          let newVote = {positive: _positive, id_person: usuario.id,  id_card: $scope.p.id,}
+          let newVote = {positive: _positive, id_person: usuario.id,  id_card: $scope.postIt.id,}
           $scope.cardMessages.vote(newVote);
         }
 
         $scope.undo = function() {
           console.log('undo');
-          $scope.p.text = JSON.parse(JSON.stringify(oldText));
+          $scope.postIt.text = JSON.parse(JSON.stringify(oldText));
           $scope.changeMode(false);
         }
 
--- Front-End/components/resultGroup/result.group.controller.js
+++ Front-End/components/resultGroup/result.group.controller.js
@@ -3,7 +3,51 @@
 
   angular
   .module('stoneBoard')
-  .controller('controllerResultGroup', function ($scope, $filter,personService) {
+  .controller('controllerResultGroup', function ($scope, $window) {
+
+  	$scope.colorPallet = ['orange', 'blue', 'pink', 'green'];
+
+  	let idealWidth = 550; //px
+  	let numSession = 2;
+    let newWidth;
+    let resized = false;
+
+    $scope.cardTest = 
+      { id: 1, 
+        text: "sdfasfasfsa", 
+        id_writer: 1, 
+        creationDate: [2017, 12, 14, 14, 18, 31, 940000000], 
+        color: 3, 
+        votes: [
+          { positive: false, 
+            id_person: 1, 
+            id_card: null
+          }
+        ]
+      }
+
+  	// verifica se houve alguma mudança na largura da tela e recalcula largura do board
+		angular.element($window).bind('resize', resizeRow);
+
+		resizeRow();
+
+  	function resizeRow() {
+
+  		let rowSession = angular.element(document.getElementById('row-sessions'));
+  		rowSession.css('width', '100%');
+
+	  	if (idealWidth > rowSession[0].clientWidth) {
+	  		newWidth = $window.innerWidth * numSession + 'px';
+	  	}
+	  	else if ( (idealWidth * numSession) >= rowSession[0].clientWidth ) {
+	  		newWidth =	(idealWidth * numSession) + 'px';
+	  	}
+	  	else {
+	  		newWidth = '100%';
+	  	}
+
+	  	rowSession.css('width', newWidth);
+  	}
 
   });
 
