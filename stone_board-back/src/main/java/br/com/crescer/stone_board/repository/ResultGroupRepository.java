766514f735c1d89f04873d0da4734b7243551618
Marcele Flores Dorneles <marcele.dorneles@cwi.com.br>
2017-12-14 12:04:14 -0200

    feat(TCC-StoneBoard): criando controller, service e repositório para Grupo de Resultado.

---
+++ stone_board-back/src/main/java/br/com/crescer/stone_board/repository/ResultGroupRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.stone_board.repository;
+
+import br.com.crescer.stone_board.entity.ResultGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author marcele.dorneles
+ */
+public interface ResultGroupRepository extends JpaRepository<ResultGroup, Long>{
+    
+}
---
+++ stone_board-back/src/main/java/br/com/crescer/stone_board/service/ResultGroupService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.stone_board.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author marcele.dorneles
+ */
+@Service
+public class ResultGroupService {
+    @Autowired
+    ResultGroupRepository resultGroupRepository;
+}
