17fd88d0ca169d09ecd928e3f3f5c24a550dd42d
Júlia Vila <juliafvila@gmail.com>
2017-12-14 16:16:58 -0200

    refactor(stoneboard): criando service para funções compartilhadas por mais de uma interface

--- Front-End/components/resultGroup/result.group.controller.js
+++ Front-End/components/resultGroup/result.group.controller.js
@@ -9,43 +9,16 @@
   	let numSession = 2;
     let newWidth;
     let resized = false;
+  	let rowSession = angular.element(document.getElementById('row-sessions'));
 
-    $scope.cardTest = 
-      { id: 1, 
-        text: "sdfasfasfsa", 
-        id_writer: 1, 
-        creationDate: [2017, 12, 14, 14, 18, 31, 940000000], 
-        color: 3, 
-        votes: [
-          { positive: false, 
-            id_person: 1, 
-            id_card: null
-          }
-        ]
-      }
+    // verifica se houve alguma mudança na largura da tela e recalcula largura do board
+    angular.element($window).bind('resize', resizeRow);
 
-  	// verifica se houve alguma mudança na largura da tela e recalcula largura do board
-		angular.element($window).bind('resize', resizeRow);
+    resizeRow();
 
-		resizeRow();
-
-  	function resizeRow() {
-
-  		let rowSession = angular.element(document.getElementById('row-sessions'));
-  		rowSession.css('width', '100%');
-
-	  	if (idealWidth > rowSession[0].clientWidth) {
-	  		newWidth = $window.innerWidth * numSession + 'px';
-	  	}
-	  	else if ( (idealWidth * numSession) >= rowSession[0].clientWidth ) {
-	  		newWidth =	(idealWidth * numSession) + 'px';
-	  	}
-	  	else {
-	  		newWidth = '100%';
-	  	}
-
-	  	rowSession.css('width', newWidth);
-  	}
+    function resizeRow(){
+      utilsService.resizeBoardWidth(rowSession, idealWidth, numSession);
+    }
 
   });
 
