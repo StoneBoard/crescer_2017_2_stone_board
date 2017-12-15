d526c0e5b01a65b298609aef7255dd4a96f0b37e
marcele.dorneles <cely.dorneles@gmail.com>
2017-12-15 03:43:19 -0200

    feat(TCC- StoneBoard): criando controller de comentario no back e service no front.

--- stone_board-back/src/main/java/br/com/crescer/stone_board/repository/NoteRepository.java
+++ stone_board-back/src/main/java/br/com/crescer/stone_board/repository/NoteRepository.java
@@ -6,6 +6,7 @@
 package br.com.crescer.stone_board.repository;
 
 import br.com.crescer.stone_board.entity.Note;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -13,5 +14,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author willian
  */
 public interface NoteRepository  extends JpaRepository<Note, Long>{
-    
+    List<Note> findByCardId(Long idCard);
 }
--- stone_board-back/src/main/java/br/com/crescer/stone_board/service/NoteService.java
+++ stone_board-back/src/main/java/br/com/crescer/stone_board/service/NoteService.java
@@ -5,12 +5,17 @@
  */
 package br.com.crescer.stone_board.service;
 
+import br.com.crescer.stone_board.entity.Card;
 import br.com.crescer.stone_board.entity.Note;
 import br.com.crescer.stone_board.entity.Person;
 import br.com.crescer.stone_board.entity.model.NoteModel;
 import br.com.crescer.stone_board.repository.CardRepository;
 import br.com.crescer.stone_board.repository.NoteRepository;
 import br.com.crescer.stone_board.repository.PersonRepository;
+import br.com.crescer.stone_board.utils.BadRequestException;
+import br.com.crescer.stone_board.utils.PersonComponent;
+import java.util.stream.Collectors;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,21 +32,38 @@ public class NoteService {
     CardRepository cardRepository;
     @Autowired
     PersonRepository personRepository;
+    @Autowired
+    PersonComponent personComponent;
     
     public void save(NoteModel noteModel) {
         Person personLoged = personRepository.findOne(noteModel.getId_writer());
         Note note = noteModel.convertToNote(noteModel, personLoged) ;
-        noteRepository.save(note);
+        Card card = cardRepository.findOne(noteModel.getId_card());
+        card.getNotes().add(note);
+        cardRepository.save(card);
     }
     
     public void update(NoteModel noteModel) {
+       Person personLoged = personComponent.loggedPersonDetails();
+        if(personLoged.getId() != noteModel.getId_writer()){
+             throw new BadRequestException("Apenas o usuário que criou o comentario pode realizar esta ação.");
+        }
         Note note = noteRepository.findOne(noteModel.getId());
         note.setText(noteModel.getText());
         noteRepository.save(note);
     }
 
     public void delete(Long id) {
+        Person personLoged = personComponent.loggedPersonDetails();
+        Note note = noteRepository.findOne(id);
         noteRepository.delete(id);
+    }
+    
+    public List<NoteModel> findByCardId(Long idCard){
+        return noteRepository.findByCardId(idCard)
+                .stream()
+                .map(NoteModel :: convertToNoteModel)
+                .collect(Collectors.toList());
     }
     
 }
